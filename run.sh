8d74cc006ee8e41aed5d1d274f27a8c363b1ff95
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2017-10-09 20:26:24 +0100

    Complete roman transducer, names need fixing

--- run.sh
+++ run.sh
@@ -7,10 +7,22 @@ python scripts/compact2fst.py mini_romanos_2.txt > mini_romanos_2_extend.txt
 fstcompile --isymbols=syms.txt --osymbols=syms.txt mini_romanos_2_extend.txt | fstarcsort > mini_romanos_2.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait mini_romanos_2.fst | dot -Tpdf > mini_romanos_2.pdf
 
+python scripts/compact2fst.py mini_romanos_3.txt > mini_romanos_3_extend.txt
+fstcompile --isymbols=syms.txt --osymbols=syms.txt mini_romanos_3_extend.txt | fstarcsort > mini_romanos_3.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait mini_romanos_3.fst | dot -Tpdf > mini_romanos_3.pdf
+
+
 #Transdutor Romano
-fstcompose mini_romanos_1.fst mini_romanos_2.fst > romanos.fst
+fstcompose mini_romanos_1.fst mini_romanos_3.fst > temp.fst
+fstcompose temp.fst mini_romanos_2.fst > romanos.fst
+
 #PDF Transdutor Romano
 fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt romanos.fst | dot -Tpdf > romanos.pdf
+
+# Transdutor 1
+#
+#
+#
 
 # Transdutor 2
 python scripts/compact2fst.py t2.txt > t2_extend.txt
