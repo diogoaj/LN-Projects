11b704c623fc50a6b087f0f62d5c2b63f515fdfb
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2017-10-10 16:42:57 +0100

    Complete all transducers

--- run.sh
+++ run.sh
@@ -20,9 +20,19 @@ fstcompose temp.fst mini_romanos_2.fst > romanos.fst
 fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt romanos.fst | dot -Tpdf > romanos.pdf
 
 # Transdutor 1
-#
-#
-#
+python scripts/compact2fst.py t1_temp1.txt > t1_temp1_extend.txt
+fstcompile --isymbols=syms.txt --osymbols=syms.txt t1_temp1_extend.txt | fstarcsort > t1_temp1.fst
+
+python scripts/compact2fst.py t1_temp2.txt > t1_temp2_extend.txt
+fstcompile --isymbols=syms.txt --osymbols=syms.txt t1_temp2_extend.txt | fstarcsort > t1_temp2.fst
+
+fstunion t1_temp1.fst romanos.fst > a.fst 
+fstconcat a.fst t1_temp2.fst > b.fst  
+fstclosure -closure_plus b.fst > transdutor1.fst
+
+
+fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt transdutor1.fst | dot -Tpdf > transdutor1.pdf
+
 
 # Transdutor 2
 python scripts/compact2fst.py t2.txt > t2_extend.txt
@@ -33,3 +43,12 @@ fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait t2.fst | dot -Tpdf
 python scripts/compact2fst.py t3.txt > t3_extend.txt
 fstcompile --isymbols=syms.txt --osymbols=syms.txt t3_extend.txt | fstarcsort > t3.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait t3.fst | dot -Tpdf > t3.pdf
+
+
+# Transdutor codificador
+fstcompose transdutor1.fst t2.fst > test1.fst 
+fstcompose test1.fst t3.fst > codifica.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait codifica.fst | dot -Tpdf > codifica.pdf
+
+# Transdutor descodifica
+fstinvert codifica.fst > descodifica.fst
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 python scripts/word2fst.py $1 > number.txt
 fstcompile --isymbols=syms.txt --osymbols=syms.txt number.txt | fstarcsort > number.fst
 
-fstcompose number.fst romanos.fst | fstshortestpath | fstproject --project_output | fstrmepsilon | fstprint --acceptor --isymbols=./syms.txt > resultado.txt
+fstcompose number.fst descodifica.fst | fstshortestpath | fstproject --project_output | fstrmepsilon | fstprint --acceptor --isymbols=./syms.txt > resultado.txt
 
 cat resultado.txt
