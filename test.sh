26718485d7b07b74045765297aa0bf4d85495b7e
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2017-10-25 23:43:51 +0100

    Remove files

--- test.sh
+++
@@ -1,6 +0,0 @@
-python scripts/word2fst.py $1 > number.txt
-fstcompile --isymbols=syms.txt --osymbols=syms.txt number.txt | fstarcsort > number.fst
-
-fstcompose number.fst codificador.fst | fstshortestpath | fstproject --project_output | fstrmepsilon | fstprint --acceptor --isymbols=./syms.txt > resultado.txt
-
-python scripts/readResult.py
