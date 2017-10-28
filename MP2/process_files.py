91c98af4c92c8f34ed0728c9082184d51785f2b8
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2017-10-28 18:22:43 +0100

    Update script

--- MP2/process_files.py
+++ MP2/process_files.py
@@ -2,13 +2,13 @@
 
 def final(f, word):
 	file_input = open(f, "r")
-	file_output = open(f.split(".")[0]+".final", "w")
+	file_output = open(f.split(".")[0]+".final", "wb")
 	
 	for line in file_input.readlines():
 		row = line.split("\t")
 		
 		if "n-é-verbo" not in row[0] and line.count(word) == 1 and "#" not in row[0] and "?" not in row[0] :
-			file_output.write(line.replace(word, row[0].lower())[(len(word)+len("\t")):])
+			file_output.write(line.replace(word, row[0].lower())[(len(row[0])+len("\t")):])
 	
 	file_input.close()
 	file_output.close()
