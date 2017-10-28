cb56829b344fe973987cf805ec1db680722325af
jvarandas <joao.varandas@tecnico.ulisboa.pt>
2017-10-28 17:53:14 +0100

    making .final generalist

--- MP2/aux1.py
+++ MP2/aux1.py
@@ -2,14 +2,15 @@
 
 def final(f, word):
 	file_input = open(f, "r")
-	file_output = open("criamCrerCriar.final", "w")
-	matrix = []
+	output = f.split(".")
+	file_output = open(output+".final", "w")
+	
 	
 	for line in file_input.readlines():
 		row = line.split("\t")
 		
-		if "n-é-verbo" not in row[0] and line.count("criam") == 1 and "#" not in row[0] and "?" not in row[0] :
-			file_output.write(line.replace("criam", "criar")[(len(word)+len("\t")):])
+		if "n-é-verbo" not in row[0] and line.count(word) == 1 and "#" not in row[0] and "?" not in row[0] :
+			file_output.write(line.replace(word, row[0])[(len(word)+len("\t")):])
 	
 	file_input.close()
 	file_output.close()
