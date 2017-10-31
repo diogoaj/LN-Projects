7271bc0d251c54610fe33c9a2f51b205e7e5f960
jvarandas <joao.varandas@tecnico.ulisboa.pt>
2017-10-31 15:02:41 +0000

    example2 of probabilities with nltk

--- MP2/compute_next_lemma.py
+++ MP2/compute_next_lemma.py
@@ -4,4 +4,6 @@ from nltk.probability import LidstoneProbDist, WittenBellProbDist
 estimator = lambda fdist, bins: LidstoneProbDist(fdist, 0.2)
 lm = NgramModel(3, brown.words(categories='news'), estimator)
 
+# https://stackoverflow.com/questions/6462709/nltk-language-model-ngram-calculate-the-prob-of-a-word-from-context
+
 print lm.prob("word", ["This is a context which generates a word"])
