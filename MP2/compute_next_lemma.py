8d488d21cc7910c08130d03b01d810f252669012
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2017-11-04 22:37:18 +0000

    Update script

--- MP2/compute_next_lemma.py
+++ MP2/compute_next_lemma.py
@@ -1,3 +1,6 @@
+from nltk import word_tokenize
+from nltk.util import ngrams
+
 def prob_laplace(wn, wnminusone, v):
 	return (float(wn)+1)/(float(wnminusone)+v)
 
@@ -25,41 +28,39 @@ def ngram_dict(ngramfile):
 
 	return res
 
-def sentence_probability(uni_dic, bi_dic, sentence, v):
-	aux = None
-
-	for wrd in sentence:
-
-			word = wrd.replace("\n", "")
-
-			if(aux == None):
-				if(word in uni_dic):
-					prob = prob_laplace(uni_dic[word], uni_dic[word], v)
-				else:
-					prob = prob_laplace(1, 1, v)
-
-			else:
-				str_aux = aux + " " + word
-
-				if(str_aux in bi_dic):
-					prob *= prob_laplace(bi_dic[str_aux], uni_dic[aux], v)
-				
-				elif(str_aux not in bi_dic and aux in uni_dic):
-					prob *= prob_laplace(1, uni_dic[aux], v)
-
-				elif(str_aux not in bi_dic and aux not in uni_dic):
-					prob *= prob_laplace(1, 0, v)
-
-			aux = word
-
-	
-	if(word in uni_dic):
-		prob *= prob_laplace(uni_dic[word], uni_dic[word], v)
+def unigram_prob(unigram, uni_dic, v):
+	if unigram in uni_dic:
+		return prob_laplace(uni_dic[unigram], uni_dic[unigram], v)
 	else:
-		prob *= prob_laplace(1, 1, v)
-	
-	return prob
+		return prob_laplace(1, 1, v)
 
+def bigram_prob(bigram, unigram, uni_dic, bi_dic, v):
+	if bigram in bi_dic and unigram in uni_dic :
+		return prob_laplace(bi_dic[bigram], uni_dic[unigram], v)
+				
+	elif bigram not in bi_dic and unigram in uni_dic:
+		return prob_laplace(1, uni_dic[unigram], v)
+
+	elif bigram not in bi_dic and unigram not in uni_dic:
+		return prob_laplace(1, 1, v)
+
+def sentence_probability(uni_dic, bi_dic, sentence, v):
+	tokens = word_tokenize(sentence)
+	unigram1 = tokens[0]
+	unigram2 = tokens[-1]
+
+	bigrams = ngrams(tokens, 2)
+
+	prob = 1
+
+	prob *= unigram_prob(unigram1, uni_dic, v)
+	prob *= unigram_prob(unigram1, uni_dic, v)
+
+	for b in bigrams:
+		bigram = b[0] + " " + b[1]
+		prob *= bigram_prob(bigram, b[0], uni_dic, bi_dic, v)
+
+	return prob
 
 def compute_lemma(unigram_file, bigram_file, param_file, sentences_file):
 
@@ -73,22 +74,18 @@ def compute_lemma(unigram_file, bigram_file, param_file, sentences_file):
 	lines = sentences.readlines()
 
 	for string in lines:
-		str1 = string.replace(param_words[2], param_words[0]).split(" ")
-		str2 = string.replace(param_words[2], param_words[1]).split(" ")
+		str1 = string.replace(param_words[2], param_words[0]).replace("\n", "")
+		str2 = string.replace(param_words[2], param_words[1]).replace("\n", "")
 
 		p1 = sentence_probability(unigram_values, bigram_values, str1, v)
 		p2 = sentence_probability(unigram_values, bigram_values, str2, v)
 
-		print p1
-		print p2
-		
 		if p1 > p2:	
 			res[string] = param_words[0]
 		else:
-			res[string] = param_words[1]
+			res[string] = param_words[1] 
 
-	print res
 	return res
 
 
-compute_lemma("palavra1Unigramas.txt", "palavra1Bigramas.txt", "fomosParametrizacao.txt", "fomos.txt")
+print compute_lemma("palavra1Unigramas.txt", "palavra1Bigramas.txt", "fomosParametrizacao.txt", "fomos.txt")
